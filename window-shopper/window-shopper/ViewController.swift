0db8c28e855f290fb0a4dac5701cd92519ed7c21
Raleigh Felton <raleighfelton@gmail.com>
2017-10-24 16:44:43 -0400

    First-commit

---
+++ window-shopper/window-shopper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Raleigh Felton on 10/24/17.
+//  Copyright © 2017 Raleigh Felton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
