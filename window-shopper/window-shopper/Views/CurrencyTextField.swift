e685c47473171334a72e8a54a82f379a31ddede3
Raleigh Felton <raleighfelton@gmail.com>
2017-10-25 10:12:01 -0400

    Styling Custom Text Field

---
+++ window-shopper/window-shopper/Views/CurrencyTextField.swift
@@ -0,0 +1,28 @@
+//
+//  CurrencyTextField.swift
+//  window-shopper
+//
+//  Created by Raleigh Felton on 10/24/17.
+//  Copyright © 2017 Raleigh Felton. All rights reserved.
+//
+
+import UIKit
+
+class CurrencyTextField: UITextField {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)
+        layer.cornerRadius = 5.0
+        textAlignment = .center
+        
+        if let p = placeholder {
+            let place = NSAttributedString(string: p, attributes: [.foregroundColor: #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)])
+            attributedPlaceholder = place
+            textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+        }
+        
+        
+    }
+
+}
